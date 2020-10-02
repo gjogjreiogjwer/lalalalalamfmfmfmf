82311a76dfba832b8e01f35f5fc342efca46a512
yicheng <yichengjin1@gmail.com>
2020-10-02 21:50:52 +1000

    first

---
+++ Pomodoro/Controller/TodoCell.swift
@@ -0,0 +1,27 @@
+//
+//  TodoCell.swift
+//  Pomodoro
+//
+//  Created by 金一成 on 2020/9/30.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    
+    @IBOutlet weak var todo: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
