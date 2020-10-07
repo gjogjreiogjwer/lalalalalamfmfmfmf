7ad78d0cb763488c855f4bc2e0c5d8b9eae41610
yicheng <yichengjin1@gmail.com>
2020-10-07 21:58:18 +0800

    fix bug of always select row

--- Pomodoro/Controller/EditController.swift
+++ Pomodoro/Controller/EditController.swift
@@ -46,6 +46,11 @@ class EditController: UITableViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
