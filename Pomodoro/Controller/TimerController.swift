2136b6e9574ea12b772757fc84ecb4674f5c57c3
yicheng <yichengjin1@gmail.com>
2020-10-07 18:31:43 +0800

    shake:themeLabel

--- Pomodoro/Controller/TimerController.swift
+++ Pomodoro/Controller/TimerController.swift
@@ -18,6 +18,8 @@ class TimerController: UIViewController {
     var timer = Timer()
     var isPlaying = false
     var delegate:TimerDelegate?
+    var i = 0
+    let themeArr = ["Spring", "Summer", "Autumn", "Winter"]
     
     
     @IBOutlet weak var timeLabel: UILabel!
@@ -58,6 +60,53 @@ class TimerController: UIViewController {
     @IBAction func done(_ sender: Any) {
         delegate?.didUpdateTime(time: Int(time))
         navigationController?.popViewController(animated: true)
+    }
+    
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        changeTheme()
+        
+        i = i >= themeArr.count-1 ? 0 : i+1
+    }
+    
+    
+    func changeTheme(){
+        let themeLabel = UILabel()
+//        themeLabel.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+        themeLabel.layer.cornerRadius = 5.0
+        themeLabel.font = UIFont.systemFont(ofSize: 30)
+        themeLabel.adjustsFontSizeToFitWidth=true
+        themeLabel.text = "Theme: \(themeArr[i])"
+        view.addSubview(themeLabel)
+        
+        themeLabel.frame.size.width = 300
+        themeLabel.frame.size.height = 100
+        themeLabel.center.x = view.center.x
+        themeLabel.center.y = view.frame.height + 50
+        
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.4,
+                       initialSpringVelocity: 10,
+                       options: [],
+                       animations: {
+                        themeLabel.frame.size.width = 600
+                        themeLabel.frame.size.height = 100
+                        themeLabel.center.x = self.view.frame.width - 10
+                        themeLabel.center.y -= 150
+                       },
+                       completion: nil)
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 2,
+                       options: .curveEaseIn) {
+            themeLabel.frame.size.width = 300
+            themeLabel.frame.size.height = 100
+            themeLabel.center.x = self.view.frame.width
+            themeLabel.center.y = self.view.frame.height + 50
+        } completion: { _ in
+            themeLabel.removeFromSuperview()
+        }
     }
     
     /*
