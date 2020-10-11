39aee6ee82b9a84e863f84f2a18f91bf9721f9b2
yicheng <yichengjin1@gmail.com>
2020-10-11 13:49:12 +0800

    main card

--- Pomodoro/Controller/MainMenuController.swift
+++ Pomodoro/Controller/MainMenuController.swift
@@ -8,50 +8,79 @@
 import UIKit
 
 class MainMenuController: UIViewController {
+    
+    let dict:[String:String] = ["List": "Each task can be added, deleted, edited, and queried. Moreover, each task takes different time to complete.", "Setting": "Timing mode selection and account management."]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         jump(text: "List", offset: 200)
-        jump(text: "Setting", offset: 80)
+        jump(text: "Setting", offset: 0)
     }
     
     
     func jump(text:String, offset:Int){
         let subView = UIView()
         subView.contentMode = .scaleToFill
-        subView.frame.size.width = 300
-        subView.frame.size.height = 100
-        subView.center.x = view.center.x - 30
+        subView.frame.size.width = 380
+        subView.frame.size.height = 180
+        subView.center.x = view.center.x
         subView.center.y = view.center.y - CGFloat(offset)
-        subView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
-        subView.layer.cornerRadius = 10.0
-
-        let image = UIImageView()
-        image.contentMode = .scaleToFill
-        image.image = UIImage(named: "mainbg")
-        image.layer.cornerRadius = 10.0
-        image.clipsToBounds = true
-        image.frame.size.width = 300
-        image.frame.size.height = 100
-        image.center.x = subView.frame.width / 2
-        image.center.y = subView.frame.height / 2
-        
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 30)
-        label.adjustsFontSizeToFitWidth=true
-        label.text = text
-        label.textColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-        label.frame.size.width = 100
-        label.frame.size.height = 50
-        label.center.x = subView.frame.width / 2 - 50
-        label.center.y = subView.frame.height / 2
-
-        subView.addSubview(image)
-        subView.insertSubview(label, aboveSubview: image)
-        view.addSubview(subView)
-        
+//        subView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        subView.layer.cornerRadius = 20.0
         subView.isUserInteractionEnabled = true
+        
+        let background = UIImageView()
+        background.contentMode = .scaleToFill
+        background.image = UIImage(named: "purple")
+        background.layer.cornerRadius = 20.0
+        background.clipsToBounds = true
+        background.frame.size.width = subView.frame.width
+        background.frame.size.height = subView.frame.height
+        background.center.x = subView.frame.width / 2
+        background.center.y = subView.frame.height / 2
+        
+        background.layer.masksToBounds = false
+        background.layer.shadowOpacity = 0.6
+        background.layer.shadowColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        background.layer.shadowOffset = CGSize(width: 10, height: 10)
+        background.layer.shadowRadius = 3
+        
+        let title = UILabel()
+        title.font = UIFont.boldSystemFont(ofSize: 40)
+        title.adjustsFontSizeToFitWidth=true
+        title.text = text
+        title.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        title.frame.size.width = 100
+        title.frame.size.height = 50
+        title.center.x = subView.frame.width / 2 - 100
+        title.center.y = subView.frame.height / 2 - 50
+        
+        let describe = UILabel()
+        describe.font = UIFont.systemFont(ofSize: 20)
+//        describe.adjustsFontSizeToFitWidth=true
+        describe.numberOfLines = 0
+        describe.text = dict[text]
+        describe.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        describe.frame.size.width = 300
+        describe.frame.size.height = 150
+        describe.center.x = subView.frame.width / 2
+        if text == "List"{
+            describe.center.y = subView.frame.height / 2 + 20
+        }
+        else if text == "Setting"{
+            describe.center.y = subView.frame.height / 2
+        }
+                
+//        describe.frame.origin.x = 50
+//        describe.frame.origin.y = 45
+//        print(describe.frame)
+//        print("xxx   \(title.frame)")
+        
+        subView.addSubview(background)
+        subView.insertSubview(title, aboveSubview: background)
+        subView.insertSubview(describe, aboveSubview: background)
+        view.addSubview(subView)
 
         if text == "List"{
             let tap = UITapGestureRecognizer(target: self, action:#selector(MainMenuController.handleTapToList(sender:)))
