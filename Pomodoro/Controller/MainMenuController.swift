b37218b3f195a13d2b41380e3f06e87771cdf8d1
yicheng <yichengjin1@gmail.com>
2020-10-11 17:38:11 +0800

    shadow

--- Pomodoro/Controller/MainMenuController.swift
+++ Pomodoro/Controller/MainMenuController.swift
@@ -10,6 +10,11 @@ import UIKit
 class MainMenuController: UIViewController {
     
     let dict:[String:String] = ["List": "Each task can be added, deleted, edited, and queried. Moreover, each task takes different time to complete.", "Setting": "Timing mode selection and account management."]
+    var shadowView:UIView!
+    var subView:UIView!
+    var background:UIImageView!
+    var subViewTitle:UILabel!
+    var describe:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,47 +23,61 @@ class MainMenuController: UIViewController {
         jump(text: "Setting", offset: 0)
     }
     
+    func createShadowView(offset:Int){
+        shadowView = UIView()
+        shadowView.frame.size.width = 380
+        shadowView.frame.size.height = 180
+        shadowView.center.x = view.center.x
+        shadowView.center.y = view.center.y - CGFloat(offset)
+        shadowView.layer.shadowColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        shadowView.layer.shadowOffset = CGSize(width: 10, height: 10)
+        shadowView.layer.shadowOpacity = 0.6
+        shadowView.layer.shadowRadius = 5.0
+        shadowView.clipsToBounds = false
+    }
     
-    func jump(text:String, offset:Int){
-        let subView = UIView()
+    
+    func createSubView(){
+        subView = UIView()
         subView.contentMode = .scaleToFill
-        subView.frame.size.width = 380
-        subView.frame.size.height = 180
-        subView.center.x = view.center.x
-        subView.center.y = view.center.y - CGFloat(offset)
-//        subView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        subView.frame.size.width = shadowView.frame.width
+        subView.frame.size.height = shadowView.frame.height
+        subView.center.x = shadowView.frame.width / 2
+        subView.center.y = shadowView.frame.height / 2
         subView.layer.cornerRadius = 20.0
         subView.isUserInteractionEnabled = true
-        
-        let background = UIImageView()
+    }
+    
+    
+    func createBackground(){
+        background = UIImageView()
         background.contentMode = .scaleToFill
         background.image = UIImage(named: "purple")
         background.layer.cornerRadius = 20.0
-        background.clipsToBounds = true
         background.frame.size.width = subView.frame.width
         background.frame.size.height = subView.frame.height
         background.center.x = subView.frame.width / 2
         background.center.y = subView.frame.height / 2
-        
-        background.layer.masksToBounds = false
-        background.layer.shadowOpacity = 0.6
-        background.layer.shadowColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        background.layer.shadowOffset = CGSize(width: 10, height: 10)
-        background.layer.shadowRadius = 3
-        
-        let title = UILabel()
-        title.font = UIFont.boldSystemFont(ofSize: 40)
-        title.adjustsFontSizeToFitWidth=true
-        title.text = text
-        title.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        title.frame.size.width = 100
-        title.frame.size.height = 50
-        title.center.x = subView.frame.width / 2 - 100
-        title.center.y = subView.frame.height / 2 - 50
-        
-        let describe = UILabel()
+        background.layer.masksToBounds = true
+    }
+    
+    
+    func createTitleLabel(text:String){
+        subViewTitle = UILabel()
+        subViewTitle.font = UIFont.boldSystemFont(ofSize: 40)
+        subViewTitle.adjustsFontSizeToFitWidth=true
+        subViewTitle.text = text
+        subViewTitle.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        subViewTitle.frame.size.width = 100
+        subViewTitle.frame.size.height = 50
+        subViewTitle.center.x = subView.frame.width / 2 - 100
+        subViewTitle.center.y = subView.frame.height / 2 - 50
+    }
+    
+    
+    func createDescribeLabel(text:String){
+        describe = UILabel()
         describe.font = UIFont.systemFont(ofSize: 20)
-//        describe.adjustsFontSizeToFitWidth=true
         describe.numberOfLines = 0
         describe.text = dict[text]
         describe.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -71,24 +90,31 @@ class MainMenuController: UIViewController {
         else if text == "Setting"{
             describe.center.y = subView.frame.height / 2
         }
-                
-//        describe.frame.origin.x = 50
-//        describe.frame.origin.y = 45
-//        print(describe.frame)
-//        print("xxx   \(title.frame)")
+    }
+    
+    
+    func jump(text:String, offset:Int){
+        createShadowView(offset: offset)
+        createSubView()
+        createBackground()
+        createTitleLabel(text: text)
+        createDescribeLabel(text: text)
         
         subView.addSubview(background)
-        subView.insertSubview(title, aboveSubview: background)
+        subView.insertSubview(subViewTitle, aboveSubview: background)
         subView.insertSubview(describe, aboveSubview: background)
-        view.addSubview(subView)
+        shadowView.addSubview(subView)
+        view.addSubview(shadowView)
 
         if text == "List"{
+            print(shadowView.frame)
             let tap = UITapGestureRecognizer(target: self, action:#selector(MainMenuController.handleTapToList(sender:)))
-            subView.addGestureRecognizer(tap)
+            shadowView.addGestureRecognizer(tap)
         }
         else if text == "Setting"{
+            print(shadowView.frame)
             let tap = UITapGestureRecognizer(target: self, action:#selector(MainMenuController.handleTapToSetting(sender:)))
-            subView.addGestureRecognizer(tap)
+            shadowView.addGestureRecognizer(tap)
         }
                                    
         
